d6da5770fd3b242bba09ba68a6e59a1483cd6f0d
Ian Fisher <ianfisher45@gmail.com>
2018-06-24 11:50:13 -0400

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,20 @@ setup(
     description='Auto-generate unit tests from interactive shell sessions',
     license='MIT',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ian Fisher',
     author_email='iafisher@protonmail.com',
     packages=find_packages(),
     classifiers=(
-        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
         'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Testing'
     ),
+    project_urls={
+        'Source': 'https://github.com/iafisher/autosuite',
+    }
 )
